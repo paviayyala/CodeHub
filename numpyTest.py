8242852b96790d0ed0440e3b3d803a16c1b2285e
Pavithran Ayyala <104089033+paviayyala@users.noreply.github.com>
2022-07-19 19:35:32 +0530

    Update numpyTest.py

--- numpyTest.py
+++ numpyTest.py
@@ -5,3 +5,28 @@ x = numpy.random.uniform(0.0, 5.0, 100000)
 
 plt.hist(x, 100)
 plt.show()
+
+
+#Execution time for both Vectorized and Non-vectorized versions. 
+ 
+import time
+ 
+a = np.random.rand(10000000)
+b = np.random.rand(10000000)
+ 
+x = time.time()
+c = np.dot(a,b)
+y = time.time()
+ 
+print(c)
+print("Vectorized version: " + str(1000*(y-x))+ "ms")
+ 
+ 
+c = 0
+x = time.time()
+for i in range(10000000):
+    c+= a[i] * b[i]
+y = time.time()
+ 
+print(c)
+print("Non-vectorized version: " + str(1000*(y-x))+"ms")
